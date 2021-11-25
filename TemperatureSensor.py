839941b998a755618a1a9d557894e777b61211c0
s49dong <s49dong@uwaterloo.ca>
2021-11-25 11:12:25 -0500

    test

--- TemperatureSensor.py
+++ TemperatureSensor.py
@@ -8,6 +8,12 @@ from notify_run import Notify
 
 #Default notification settings for texts to your phone
 notification = Notify
+(
+   default_notification_title="Covid-Robot Alert",
+   # default_application_name="Great Application",
+   # default_notification_icon="path/to/icon.png",
+   # default_notification_audio="path/to/sound.wav"
+ )
 
 bus = SMBus(1)
 yellow = LED(14)
@@ -32,8 +38,8 @@ print("")
 t = time.localtime()
 timeNow = time.strftime("%H:%M:%S", t)
 
-notification.message = "Testing: ", timeNow," EST"
-notification.send()
+#notification.message = "Testing: ", timeNow," EST"
+notification.send("Hello"); 
 if(wristTemp<30):
     yellow.on()
     print("Temperature too low: ", wristTemp, " *C \n")
