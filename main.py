480b4b8e70f247818ce41c74ba997508255639d1
Kimmy <kmko@uwaterloo.ca>
2021-11-25 11:52:53 -0500

    importing files

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from detect_mask_outsidefile.py import mask_on
+#import facemask_detection
+#import TemperatureSensor
+
+exec(open('detect_mask.py').read())
+
+
 
 if __name__ == "__main__":
     ans = mask_on
