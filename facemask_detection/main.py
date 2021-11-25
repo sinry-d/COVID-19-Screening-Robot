8c45340509bec7f129af82fb10285619ca5b0099
Kimmy <kmko@uwaterloo.ca>
2021-11-25 17:29:51 -0500

    added capacity counter

--- facemask_detection/main.py
+++ facemask_detection/main.py
@@ -1,10 +1,29 @@
 import detect_mask
 import TemperatureSensor
 
-#detect_mask.detectmask()
-goodtemp = TemperatureSensor.tempsensor()
-maskon = detect_mask.detectmask()
+max_capacity = 2
+current_capacity = 0
+goodtemp = False
+maskon = False
 
-print ("temp good:",goodtemp)
-print ("mask good:", maskon)
+while True:
+    maskon = detect_mask.detectmask()
+    if maskon == True:
+        goodtemp = TemperatureSensor.tempsensor()
+
+    print ("temp good:",goodtemp)
+    print ("mask good:", maskon)
+
+    if (maskon and goodtemp and (current_capacity<max_capacity)):
+        print ("You are safe to enter")
+        current_capacity += 1
+        maskon = False
+        goodtemp = False
+    else:
+        print ("You may not enter")
+        if current_capacity == max_capacity:
+            print("Please wait for someone to leave before entering")
+        maskon = False
+        goodtemp = False
+
 
